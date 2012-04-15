d879a2c9ad04f72816570bac3be6c6852d002df0
Han Wang <hanwang.work@gmail.com>
2012-04-16 01:29:51 +0800

    refactor banner.php

---
+++ banner.php
@@ -0,0 +1,9 @@
+<tr>
+	<td background="images/flash1.jpg">
+	<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" width="784" height="170">
+		<param name="movie" value="images/1.swf" />
+		<param name="quality" value="high" />
+		<embed src="images/1.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="784" height="170"></embed>
+		</object>
+	</td>
+</tr>
