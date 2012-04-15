a7a759e3f149fd63cd663cdf8090b1157a1287b5
Han Wang <hanwang.work@gmail.com>
2012-04-16 01:00:30 +0800

    refactor footer and header

---
+++ footer.php
@@ -0,0 +1,18 @@
+<table width="100%" height="60" border="0" cellpadding="0" cellspacing="0" bgcolor="#f7f7f7">
+	<tr>
+      <td><table width="75%"  border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr align="left" valign="middle">
+          <td width="479" height="49">
+          	<div style="padding-left:57px; padding-top:0px">
+          		<span class="style1">
+          		<a href="index.php" class="style2">首页</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          		<a href="about.php" class="style2">关于瑞特</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          		<a href="service.php" class="style2">服务项目</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          		<a href="hr.php" class="style2">人才招聘</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          		<a href="contact.php" class="style2">联系我们</a></span></div></td>
+          <td width="190" class="style1">
+          	<span class="style2">&copy; 2008</span>&nbsp;瑞特口腔&nbsp;|&nbsp;&nbsp;<a href="mianze.php" class="style2">免责条款</a></td>
+        </tr>
+      </table></td>
+    </tr>
+</table>
---
+++ header.php
@@ -0,0 +1,26 @@
+   <table width="784" height="48" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td width="530">&nbsp;</td>
+          <td width="254"><table width="300" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <td width="15"><img src="images/icon01.gif" width="9" height="10" /></td>
+              <td><a href="index.php">回到首页</a></td>
+              <td width="13"><img src="images/icon04.gif" width="3" height="11" /></td>
+              <td width="15"><img src="images/icon02.gif" width="9" height="10" /></td>
+              <td><a href="contact.php">联系我们</a></td>
+              <td width="13"><img src="images/icon04.gif" width="3" height="11" /></td>
+              <td width="15"><img src="images/icon03.gif" width="11" height="9" /></td>
+              <td><a href="map.php">网站地图</a></td>
+            </tr>
+          </table></td>
+        </tr>
+        </table>
+      <table width="784" height="83" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" width="784" height="83">
+            <param name="movie" value="images/new.swf" />
+            <param name="quality" value="high" />
+            <embed src="images/new.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="784" height="83"></embed>
+          </object></td>
+          </tr>
+        </table>
