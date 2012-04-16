98dbbb8c77b24c7b7a77e68f50f692f594ba7f0a
Han Wang <hanwang.work@gmail.com>
2012-04-17 02:00:53 +0800

    Revert "Revert "extract navigator.php from header.php""
    This reverts commit 5224a7beb3ea2c926539af2c682efdba5ee2fbb7.

--- header.php
+++ header.php
@@ -14,13 +14,4 @@
             </tr>
           </table></td>
         </tr>
-        </table>
-      <table width="784" height="83" border="0" cellpadding="0" cellspacing="0">
-        <tr>
-          <td><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" width="784" height="83">
-            <param name="movie" value="images/new.swf" />
-            <param name="quality" value="high" />
-            <embed src="images/new.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="784" height="83"></embed>
-          </object></td>
-          </tr>
-        </table>
+</table>
