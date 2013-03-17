ac314089d8019bdfad860c8e7d2c11a08aee312b
hanwang <hanwang.work@gmail.com>
2013-03-17 14:41:16 +0800

    about right page

---
+++ aboutrt.php
@@ -0,0 +1,51 @@
+<?php
+	require_once("loadhead.php");
+?>
+
+<?php MasterPage::BeginPlaceHolder(); ?>
+关于瑞特-诚聘英才
+<?php MasterPage::EndPlaceHolder("title"); ?>
+
+<?php MasterPage::BeginPlaceHolder(); ?>
+<img src="images/right_01.jpg" width="220" height="48" alt="" />
+<?php MasterPage::EndPlaceHolder("image1"); ?>
+
+<?php MasterPage::BeginPlaceHolder(); ?>
+<img src="images/right_02.jpg" width="220" height="83" alt="" />
+<?php MasterPage::EndPlaceHolder("image2"); ?>
+
+<?php MasterPage::BeginPlaceHolder(); ?>
+<img src="images/right02_03.jpg" width="220" height="170" alt="" />
+<?php MasterPage::EndPlaceHolder("image3"); ?>
+
+<?php MasterPage::BeginPlaceHolder(); ?>
+<img src="images/right02_04.jpg" width="220" height="388" alt="" />
+<?php MasterPage::EndPlaceHolder("image4"); ?>
+
+<?php MasterPage::BeginPlaceHolder(); ?>
+<table width="100%" border="0" align="center" cellpadding="4" cellspacing="0">
+<tbody>
+	<tr>
+		<td colspan="2">&nbsp;&nbsp;<img src="images/pz.jpg" width="200" height="42" /></td>
+	</tr>
+	<tr>
+	</tr>
+</tbody>
+</table>
+	<div class="container-fluid">
+	<div class="row-fluid">
+	  <div class="span7">
+            <p>　　作为专业的口腔医疗机构，瑞特口腔致力于为每一位客户提供最专业的治疗计划和最全面的解决方案。瑞特口腔拥有一支技术成熟的医护团队，可进行全面的治疗操作。诊所全部采用国际高端的专业诊疗设备，所用的器械和材料均为国外知名公司的专业产品。专业的技术操作和国际标准的消毒流程更能为整个治疗提供安全保障。 <br />
+                                　　注重细节不仅是行业本身的特殊性要求，同时也是瑞特口腔长期发展的理念。瑞特口腔的每一位医护人员都将通过细节化操作、细节化管理、细节化服务，真诚地为客户提供高品质的人性化服务，让您感受到与众不同的治疗体验。 <br />
+                                　　因为我们深信 -- <img src="images/text02.jpg" width="101" height="19" align="absmiddle" /><br />
+            </p>
+            
+            </div>
+            <div class="span5"><img width="270" height="180" class="floatRightImage" id="_x0000_i1038" src="images/info/about.jpg"></div>
+        </div></div>
+            
+	<br/>
+</div>
+<?php MasterPage::EndPlaceHolder("content"); ?>
+
+<?php MasterPage::Render("master.php"); ?>
