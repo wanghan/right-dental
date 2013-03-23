ec8a76c95bbb6fec7f903b5d24b4661bd82751b3
hanwang <hanwang.work@gmail.com>
2013-03-24 04:12:16 +0800

    disinfection page

---
+++ disinfection.php
@@ -0,0 +1,149 @@
+<?php
+	require_once("loadhead.php");
+?>
+
+<?php MasterPage::BeginPlaceHolder(); ?>
+关于瑞特-严谨的消毒流程
+<?php MasterPage::EndPlaceHolder("title"); ?>
+
+<?php MasterPage::BeginPlaceHolder(); ?>
+<img src="images/right_01.jpg" width="220" height="48" alt="" />
+<?php MasterPage::EndPlaceHolder("image1"); ?>
+
+<?php MasterPage::BeginPlaceHolder(); ?>
+<img src="images/right_02.jpg" width="220" height="83" alt="" />
+<?php MasterPage::EndPlaceHolder("image2"); ?>
+
+<?php MasterPage::BeginPlaceHolder(); ?>
+<img src="images/right02_03.jpg" width="220" height="170" alt="" />
+<?php MasterPage::EndPlaceHolder("image3"); ?>
+
+<?php MasterPage::BeginPlaceHolder(); ?>
+<img src="images/right02_04.jpg" width="220" height="388" alt="" />
+<?php MasterPage::EndPlaceHolder("image4"); ?>
+
+<?php MasterPage::BeginPlaceHolder(); ?>
+<table width="100%" border="0" align="center" cellpadding="4" cellspacing="0">
+<tbody>
+	<tr>
+		<td colspan="2">&nbsp;&nbsp;<img src="images/info/disinfection.png" width="200" height="42" /></td>
+	</tr>
+	<tr>
+	</tr>
+</tbody>
+</table>
+	<div class="container-fluid">
+	<div class="row-fluid">
+            <strong><h2>消毒室内器械消毒</h2></strong>
+            <p>遵循一人一用一灭菌的原则，每个患者使用过的器械经过消毒液浸泡——清洗——含酶洗液超声振荡——清洗干燥——器械保养——单独包装——真空灭菌——无菌柜储存。定期进行细菌芽孢试验，检测锅的消毒效果</p>
+        </div>
+            <div class="row-fluid">
+            <div id="gallery" class="ad-gallery">
+                <div class="ad-image-wrapper">
+                </div>
+                <div class="ad-controls">
+                </div>
+                <div class="ad-nav">
+                  <div class="ad-thumbs">
+                    <ul class="ad-thumb-list">
+                      <li>
+                        <a href="images/info/disinfection/dis11.jpg">
+                          <img src="images/info/disinfection/dis11_t.jpg" title="消毒液浸泡器械" class="image0">
+                        </a>
+                      </li>
+                      <li>
+                        <a href="images/info/disinfection/dis12.jpg">
+                          <img src="images/info/disinfection/dis12_t.jpg" title="超声清洗" class="image1">
+                        </a>
+                      </li>
+                      <li>
+                        <a href="images/info/disinfection/dis13.jpg">
+                          <img src="images/info/disinfection/dis13_t.jpg" title="流动水认真清洗器械的每一个缝隙不留死角" class="image2">
+                        </a>
+                      </li>
+                      <li>
+                        <a href="images/info/disinfection/dis14.jpg">
+                          <img src="images/info/disinfection/dis14_t.jpg" title="器械独立密封" class="image3">
+                        </a>
+                      </li>
+                      <li>
+                        <a href="images/info/disinfection/dis15.jpg">
+                          <img src="images/info/disinfection/dis15_t.jpg" title="高温高压消毒" class="image4">
+                        </a>
+                      </li>
+                      <li>
+                        <a href="images/info/disinfection/dis16.jpg">
+                          <img src="images/info/disinfection/dis16_t.jpg" title="消毒后无菌储存" class="image5">
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+	<br/>
+        
+        <div class="container-fluid">
+	<div class="row-fluid">
+          <strong><h2>牙椅的消毒</h2></strong>
+            <p>每个患者治疗结束后，我们都会用专业的消毒液擦拭牙椅及治疗工作中有可能接触的位置，避免交叉感染</p>
+            <p>每日紫外线空气消毒一小时</p>
+        </div>
+            <div class="row-fluid">
+            <div id="gallery1" class="ad-gallery">
+                <div class="ad-image-wrapper">
+                </div>
+                <div class="ad-controls">
+                </div>
+                <div class="ad-nav">
+                  <div class="ad-thumbs">
+                    <ul class="ad-thumb-list">
+                      <li>
+                        <a href="images/info/disinfection/dis21.png">
+                          <img src="images/info/disinfection/dis21_t.png" class="image10">
+                        </a>
+                      </li>
+                      <li>
+                        <a href="images/info/disinfection/dis22.png">
+                          <img src="images/info/disinfection/dis22_t.png" class="image11">
+                        </a>
+                      </li>
+                      <li>
+                        <a href="images/info/disinfection/dis23.png">
+                          <img src="images/info/disinfection/dis23_t.png" class="image12">
+                        </a>
+                      </li>
+                      <li>
+                        <a href="images/info/disinfection/dis24.png">
+                          <img src="images/info/disinfection/dis24_t.png" class="image13">
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+	<div class="row-fluid">
+	  <div class="span7">
+            <p>在治疗工作中有接触的地方，如灯柄、手控开关等地我们都会用防污膜覆盖，保证您的安全治疗</p>
+            </div>
+          <div class="span5"><img width="270" height="180" class="floatRightImage" id="_x0000_i1038" src="images/info/disinfection/dis2.jpg"></div>
+        </div></div>
+        
+        <br/>
+        <div class="container-fluid">
+	<div class="row-fluid">
+	  <div class="span7">
+            <strong><h2>医护人员手消毒</h2></strong>
+            <p>瑞特口腔使用美国FDA唯一审批用于外科洗手的免洗手消毒液进行医护人员手的清洁消毒，每个患者治疗开始前，医护人员都会用专业的外科手消毒液进行洗手</p>
+            </div>
+            <div class="span5"><img width="270" height="180" class="floatRightImage" id="_x0000_i1038" src="images/info/disinfection/dis1.png"></div>
+        </div></div>
+        <br/>
+</div>
+<?php MasterPage::EndPlaceHolder("content"); ?>
+
+<?php MasterPage::Render("master.php"); ?>
